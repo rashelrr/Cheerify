1a17c3e866b4065e8dcb3a88732bca0896b0b174
Rashel Rojas <rdr2139@columbia.edu>
2024-01-05 17:33:56 -0500

    First half of code for Day 1

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,93 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 242/255, green: 237/255, blue: 228/255)
+                .ignoresSafeArea()
+            VStack {
+                HStack {
+                    // Menu button
+                    Button {
+                        print("Button")
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                            .frame(width: 50, height: 50)
+                            .background(Color(red: 196/255, green: 197/255, blue: 202/255))
+                            .clipShape(Circle())
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                    }
+                    .padding(.leading, 10)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("Cheerify")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(red: 63/255, green: 65/255, blue: 78/255))
+                        Text("daily affirmation companion")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color(red: 63/255, green: 65/255, blue: 78/255))
+                    }
+                    
+                    Spacer()
+                    
+                    // "Add" affirmation button
+                    Button {
+                        print("Button")
+                    } label: {
+                        Image(systemName: "plus")
+                            .frame(width: 50, height: 50)
+                            .background(Color(red: 196/255, green: 197/255, blue: 202/255))
+                            .clipShape(Circle())
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                    }
+                    .padding(.trailing, 10)
+                }
+                
+                Spacer()
+                Text("This is my affirmation, yay")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 63/255, green: 65/255, blue: 78/255))
+                    .multilineTextAlignment(.center)
+                Spacer()
+                    
+                HStack {
+                    // Refresh affirmation button
+                    Button {
+                        print("Button")
+                    } label: {
+                        Image(systemName: "arrow.clockwise")
+                            .frame(width: 50, height: 50)
+                            .background(Color(red: 196/255, green: 197/255, blue: 202/255))
+                            .clipShape(Circle())
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                    }
+                    .padding(.leading, 10)
+                    
+                    Spacer()
+                    
+                    // Favorite affirmation button
+                    Button {
+                        print("Button")
+                    } label: {
+                        Image(systemName: "heart.fill")
+                            .frame(width: 50, height: 50)
+                            .background(Color(red: 196/255, green: 197/255, blue: 202/255))
+                            .clipShape(Circle())
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                    }
+                    .padding(.trailing, 10)
+                }
+                
+            }
+        }
     }
 }
 
