4ab0eb82e15f5e4b919a69fe038b215518e83e11
Rashel Rojas <rdr2139@columbia.edu>
2024-01-05 15:54:36 -0500

    Initial Commit

---
+++ Shared/CheerifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheerifyApp.swift
+//  Shared
+//
+//  Created by Rashel Rojas on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CheerifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
